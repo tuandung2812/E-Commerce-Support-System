b1fdb1b35d67ed9160b8c873c14d429138ee3ea5
vietcheems <731pkmn@gmail.com>
2022-12-25 01:25:26 +0700

    add restart driver method

--- src/scraper/common_scraper.py
+++ src/scraper/common_scraper.py
@@ -9,16 +9,20 @@ logger = logging.getLogger(__name__)
 
 class CommonScraper(ABC):
 
-    def __init__(self, num_page, data_dir, wait_timeout, retry_num):
+    def __init__(self, num_page, data_dir, wait_timeout, retry_num,  restart_num):
+        self.restart_num = restart_num
         self.num_page = num_page
         self.data_dir = data_dir
         self.wait_timeout = wait_timeout
         self.retry_num = retry_num
+        self.driver = self.start_driver()
 
+    def start_driver(self):
         options = uc.ChromeOptions()
-        options.set_capability("pageLoadStrategy", "eager")
+        options.set_capability("pageLoadStrategy", "none")
         driver = uc.Chrome(options=options)
-        self.driver = driver
+        driver.set_script_timeout(10)
+        return driver
 
     def write_to_file(self, text, file_name):
         full_path = os.path.join(self.data_dir, file_name)
@@ -32,6 +36,11 @@ class CommonScraper(ABC):
     @abstractmethod
     def get_product_info(self):
         pass
+
+    def restart_driver(self):
+        self.driver.quit()
+        self.driver = self.start_driver()
+        logger.info('Driver restarted')
 
     def __del__(self):
         self.driver.quit()
