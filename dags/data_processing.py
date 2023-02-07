a0e432997c3163de704cf4ca2a74f284392f2529
jazz Dũng <dungpham.020901@gmail.com>
2023-02-07 15:40:02 +0700

    hdfs babyyy

--- dags/data_processing.py
+++ dags/data_processing.py
@@ -19,18 +19,28 @@ with DAG(
 ) as dag:
 
     task1 = BashOperator(
+        task_id='crawl_url',
+        bash_command='python 3 /home/jazzdung/E-Commerce-Support-System/script/main.py --site shopee --type url --num_page 5'
+    )
+
+    task2 = BashOperator(
+        task_id='crawl_data',
+        bash_command='python 3 /home/jazzdung/E-Commerce-Support-System/script/main.py --site shopee --type info'
+    )
+
+    task3 = BashOperator(
         task_id='clean_data',
         bash_command='python3 /home/jazzdung/E-Commerce-Support-System/script/full_data.py --origin /home/jazzdung/data/product.ndjson --destination /home/jazzdung/data/full_data.csv'
     )
 
-    task2 = BashOperator(
+    task4 = BashOperator(
         task_id='create_visualize_data',
         bash_command='python3 /home/jazzdung/E-Commerce-Support-System/script/visualize_data.py --origin /home/jazzdung/data/full_data.csv --destination /home/jazzdung/data/visualiza_data.csv'
     )
 
-    task3 = BashOperator(
+    task5 = BashOperator(
         task_id='create_model_data',
         bash_command='python3 /home/jazzdung/E-Commerce-Support-System/script/model_data.py --origin /home/jazzdung/data/full_data.csv --destination /home/jazzdung/data/model_data.csv'
     )
 
-    task1>>task2>>task3
+    task1>>task2>>task3>>[task4,task5]
